6a2628f4a780e0241a488d7b064659a849fb793a
ashish <ntgupta866@gmail.com>
2017-06-20 20:56:22 +0530

    New Commit

--- app/src/main/java/com/jkpaper/jksales/Activity/MainActivity.java
+++ app/src/main/java/com/jkpaper/jksales/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jkpaper.jksales;
+package com.jkpaper.jksales.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jkpaper.jksales.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/jkpaper/jksales/Models/ResponseInterface.java
@@ -0,0 +1,8 @@
+package com.jkpaper.jksales.Models;
+
+/**
+ * Created by ashish on 20/6/17.
+ */
+
+public class ResponseInterface {
+}
